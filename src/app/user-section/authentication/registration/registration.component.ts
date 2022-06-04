52e9606079de805c883cfc47be2b0c47a92b7c3f
Parsley <grigorashnazar@gmail.com>
2022-06-04 16:29:35 +0300

    login and reg

--- src/app/user-section/authentication/registration/registration.component.ts
+++ src/app/user-section/authentication/registration/registration.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthenticationService} from "../../../services/authentication.service";
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {Router} from "@angular/router";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-registration',
@@ -6,10 +10,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent implements OnInit {
+  form: FormGroup = {} as FormGroup;
 
-  constructor() { }
+  constructor(
+    private auth: AuthenticationService,
+    private formBuilder: FormBuilder,
+    private http: HttpClient,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      firstName: '',
+      lastName: '',
+      login: '',
+      password: '',
+      rpassword: ''
+    })
   }
 
+  submit(): void{
+    const data = this.form.getRawValue();
+
+    if(data.password != data.rpassword || data.password.length < 8){
+      alert('Incorrect password')
+      return;
+    }
+
+    delete data.rpassword;
+
+    this.auth.registration(data).subscribe({
+      next: () => {
+        this.router.navigate(['login']);
+      },
+      error: err => {
+        if(err.status === 403){
+          alert('Wrong Data');
+        }
+      }
+    })
+  }
 }
